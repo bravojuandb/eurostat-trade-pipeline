c435533a12abf0ff8948b649d241dd940e74aa8b
Juan David Bravo <196306536+bravojuandb@users.noreply.github.com>
2026-03-18 07:06:02 +0100

    test(silver): add test_count_input_dat_files

---
+++ tests/silver/test_transform_unit.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import pytest
+
+from src.silver.transform import (
+    count_input_dat_files
+)
+
+def create_dat_file(folder: Path, month: str):
+    month_dir = folder / month
+    month_dir.mkdir()
+    (month_dir / "test.dat").write_text(
+        "REPORTER|PARTNER|PRODUCT_NC|FLOW|PERIOD|VALUE_EUR|QUANTITY_KG\n"
+    )
+
+def test_count_input_dat_files(tmp_path):
+
+    months = ["2004-01", "2004-02", "2004-03", "2004-05", "2004-06"]
+
+    for month in months:
+        create_dat_file(tmp_path, month)
+    
+    files = count_input_dat_files(tmp_path, "2004-01", "2004-06")
+
+    assert  len(files) == len(months)
+
